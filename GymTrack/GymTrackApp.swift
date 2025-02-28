3e52c220433e3e03026fb01e167ee0df1ab91ef4
SergioComeron <sergiocomeron@icloud.com>
2025-02-28 19:14:44 +0100

    Initial Commit

---
+++ GymTrack/GymTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymTrackApp.swift
+//  GymTrack
+//
+//  Created by Sergio Comerón on 28/2/25.
+//
+
+import SwiftUI
+
+@main
+struct GymTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GymTrackTests/GymTrackTests.swift
@@ -0,0 +1,17 @@
+//
+//  GymTrackTests.swift
+//  GymTrackTests
+//
+//  Created by Sergio Comerón on 28/2/25.
+//
+
+import Testing
+@testable import GymTrack
+
+struct GymTrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
